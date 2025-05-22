13dcbab157c59c23b268d7889fd40dd27ec0a5ea
Entropie67 <elophe.olivier@gmail.com>
2025-05-22 13:31:21 +0200

    affichage du fond

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ largeur, hauteur = 800, 600
 fenetre = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption('Fenêtre Pygame')
 
+#chargement d'une image :
+
+# Chargement de l'image de fond
+fond = pygame.image.load('images/fonds/fond1.jpg').convert()
+fond = pygame.transform.scale(fond, (largeur, hauteur))
+
+
 # Boucle principale
 en_cours = True
 while en_cours:
@@ -16,8 +23,10 @@ while en_cours:
         if evenement.type == QUIT:
             en_cours = False
 
+    
+    # Affichage de l'image de fond
+    fenetre.blit(fond, (0, 0))
     # Rafraîchir l'affichage
-    fenetre.fill((0, 0, 0))  # fond noir
     pygame.display.flip()
 
 # Fermeture de Pygame
